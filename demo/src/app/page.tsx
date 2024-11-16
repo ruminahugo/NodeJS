6a2875b1c3350b937f1d843921ed7dd66cb853ce
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2024-11-16 11:42:16 +0700

    Update page.tsx

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -10,11 +10,11 @@ export default function Home(/*{ searchParams }: { searchParams: { user?: string
         <p>User parameter is missing.</p>
       </div>
     );
-  }
+  }*/
 
   return (
     <div className="grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <CustomerPage username={username} />
+      
     </div>
-  ); */
+  );
 }
