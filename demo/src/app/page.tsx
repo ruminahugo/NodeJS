14b66fe6eeb3583c48acd5f1e793738776c0a455
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2024-11-16 11:51:54 +0700

    Update page.tsx

--- demo/src/app/page.tsx
+++ demo/src/app/page.tsx
@@ -1,10 +1,20 @@
+import CustomerPage from "@/app/page1";
 
+export default function Home({ searchParams }: { searchParams: { user?: string } }) {
+  const username = searchParams?.user;
 
-export default function Home() {
-  
+  if (!username) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <p>User parameter is missing.</p>
+      </div>
+    );
+  }
+
   return (
     <div className="grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
+      <CustomerPage username={username} />
     </div>
   );
 }
