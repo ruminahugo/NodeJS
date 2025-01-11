b4e1cab96f603e0219a429a44f366f630d256386
ruminahugo <tinhyeuthuhanvasutradua@gmail.com>
2025-01-11 12:08:10 +0700

    Update server.js

--- API/src/server.js
+++ API/src/server.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 connectDb();
 
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-/*app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});*/
+});
